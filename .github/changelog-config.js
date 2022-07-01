2f96f80cc25ca40bb75de4ed12cb63910f859b46
Molly Depew <molly.depew@zebra.com>
2022-07-01 15:44:51 -0400

    fix: config

--- .github/changelog-config.js
+++ .github/changelog-config.js
@@ -1,15 +1,15 @@
 module.exports = {
     types: [
-      { types: ['feat', 'feature'], label: '🎉 New Features' },
-      { types: ['fix', 'bugfix'], label: '🐛 Bugfixes' },
-      { types: ['improvements', 'enhancement'], label: '🔨 Improvements' },
-      { types: ['perf'], label: '🏎️ Performance Improvements' },
-      { types: ['build', 'ci'], label: '🏗️ Build System' },
+      { types: ['feat', 'feature'], label: 'New Features' },
+      { types: ['fix', 'bugfix'], label: 'Bugfixes' },
+      { types: ['improvements', 'enhancement'], label: 'Improvements' },
+      { types: ['perf'], label: 'Performance Improvements' },
+      { types: ['build', 'ci'], label: 'Build System' },
       { types: ['refactor'], label: '🪚 Refactors' },
       { types: ['doc', 'docs'], label: '📚 Documentation Changes' },
-      { types: ['test', 'tests'], label: '🔍 Tests' },
-      { types: ['style'], label: '💅 Code Style Changes' },
-      { types: ['chore'], label: '🧹 Chores' },
+      { types: ['test', 'tests'], label: 'Tests' },
+      { types: ['style'], label: 'Code Style Changes' },
+      { types: ['chore'], label: 'Chores' },
       { types: ['other'], label: 'Other Changes' },
     ],
   
